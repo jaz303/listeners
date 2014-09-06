1ece0af925d93ec45ed19e4153f9d4ea9c0ebbd7
Jason Frame <jason@onehackoranother.com>
2014-09-06 13:17:29 +0100

    tabs -> spaces

--- test/all.js
+++ test/all.js
@@ -3,146 +3,146 @@ var Listeners = require('..');
 
 test('add, fire', function(assert) {
 
-	var ls = new Listeners();
+    var ls = new Listeners();
 
-	var called = false;
-	ls.add(function() { called = true; });
+    var called = false;
+    ls.add(function() { called = true; });
 
-	ls.fire();
-	assert.ok(called);
+    ls.fire();
+    assert.ok(called);
 
-	assert.end();
+    assert.end();
 
 });
 
 test('fire - multiple', function(assert) {
 
-	var ls = new Listeners();
+    var ls = new Listeners();
 
-	var i = 0;
-	ls.add(function() { i += 1; });
-	ls.add(function() { i += 2; });
-	ls.add(function() { i += 3; });
+    var i = 0;
+    ls.add(function() { i += 1; });
+    ls.add(function() { i += 2; });
+    ls.add(function() { i += 3; });
 
-	ls.fire();
-	assert.equal(i, 6);
+    ls.fire();
+    assert.equal(i, 6);
 
-	assert.end();
+    assert.end();
 
 });
 
 test('fire - context', function(assert) {
 
-	var ls = new Listeners();
+    var ls = new Listeners();
 
-	var foo = { x: 0 }
-	ls.add(function() { this.x += 1; }, foo);
+    var foo = { x: 0 }
+    ls.add(function() { this.x += 1; }, foo);
 
-	ls.fire();
-	assert.equal(foo.x, 1);
+    ls.fire();
+    assert.equal(foo.x, 1);
 
-	assert.end();
+    assert.end();
 
 });
 
 test('fire - arg passing', function(assert) {
 
-	var ls = new Listeners();
+    var ls = new Listeners();
 
-	var out = 0;
-	ls.add(function(a, b) { out = a + b; });
+    var out = 0;
+    ls.add(function(a, b) { out = a + b; });
 
-	ls.fire(10, 5);
-	assert.equal(out, 15);
+    ls.fire(10, 5);
+    assert.equal(out, 15);
 
-	assert.end();
+    assert.end();
 
 });
 
 test('fire array - context', function(assert) {
 
-	var ls = new Listeners();
+    var ls = new Listeners();
 
-	var foo = { x: 0 }
-	ls.add(function() { this.x += 1; }, foo);
+    var foo = { x: 0 }
+    ls.add(function() { this.x += 1; }, foo);
 
-	ls.fireArray([]);
-	assert.equal(foo.x, 1);
+    ls.fireArray([]);
+    assert.equal(foo.x, 1);
 
-	assert.end();
+    assert.end();
 
 });
 
 test('fire array - arg passing', function(assert) {
 
-	var ls = new Listeners();
+    var ls = new Listeners();
 
-	var out = 0;
-	ls.add(function(a, b) { out = a + b; });
+    var out = 0;
+    ls.add(function(a, b) { out = a + b; });
 
-	ls.fireArray([10, 5]);
-	assert.equal(out, 15);
+    ls.fireArray([10, 5]);
+    assert.equal(out, 15);
 
-	assert.end();
+    assert.end();
 
 });
 
 test('remove - removes one listener only', function(assert) {
 
-	var ls = new Listeners();
+    var ls = new Listeners();
 
-	var x = 0;
-	var fn = function() { x++; };
+    var x = 0;
+    var fn = function() { x++; };
 
-	ls.add(fn);
-	ls.add(fn);
-	ls.add(fn);
+    ls.add(fn);
+    ls.add(fn);
+    ls.add(fn);
 
-	ls.remove(fn);
+    ls.remove(fn);
 
-	ls.fire();
-	assert.equal(x, 2);
+    ls.fire();
+    assert.equal(x, 2);
 
-	assert.end();
+    assert.end();
 
 });
 
 test('remove - removes correct context', function(assert) {
 
-	var ls = new Listeners();
+    var ls = new Listeners();
 
-	var a = { x: 0 };
-	var b = { x: 0 };
-	var fn = function() { this.x++; };
+    var a = { x: 0 };
+    var b = { x: 0 };
+    var fn = function() { this.x++; };
 
-	ls.add(fn, a);
-	ls.add(fn, b);
+    ls.add(fn, a);
+    ls.add(fn, b);
 
-	ls.remove(fn, a);
+    ls.remove(fn, a);
 
-	ls.fire();
+    ls.fire();
 
-	assert.equal(a.x, 0);
-	assert.equal(b.x, 1);
+    assert.equal(a.x, 0);
+    assert.equal(b.x, 1);
 
-	assert.end();
+    assert.end();
 
 });
 
 test('clear', function(assert) {
 
-	var ls = new Listeners();
+    var ls = new Listeners();
 
-	var i = 0;
-	ls.add(function() { i += 1; });
-	ls.add(function() { i += 2; });
-	ls.add(function() { i += 3; });
+    var i = 0;
+    ls.add(function() { i += 1; });
+    ls.add(function() { i += 2; });
+    ls.add(function() { i += 3; });
 
-	ls.clear();
+    ls.clear();
 
-	ls.fire();
-	assert.equal(i, 0);
+    ls.fire();
+    assert.equal(i, 0);
 
-	assert.end();
+    assert.end();
 
 });
